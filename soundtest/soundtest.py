49ac4ca3297c8015abaf9612cef87f1ae4aab50d
Eric Shaw <shaweric01@gmail.com>
2018-02-08 01:05:59 +0800

    small stuff

--- soundtest/soundtest.py
+++ soundtest/soundtest.py
@@ -1,10 +1,12 @@
 from winsound import Beep
+from time import sleep
 import argparse
 
 
 def beep_it():
     for factor in range(1, 20):
         Beep(factor * 100, 200)
+        sleep(0.15)
 
 
 def beep_for_times(times):
@@ -20,6 +22,6 @@ def main():
 
     beep_for_times(args.num)
 
+
 if __name__ == '__main__':
     main()
-
